b08973173e284b4ad97faf317ae2b863aff67cb9
Caine Benoy <cainebenoy@gmail.com>
2024-11-29 09:02:21 +0530

    Update ocr.py

--- ocr.py
+++ ocr.py
@@ -2,7 +2,7 @@ from pytesseract import image_to_string
 from pdf2image import convert_from_path
 
 def ocr_answer_sheet(pdf_path):
-    # Convert PDF to images
+    # Convert PDF to images and extract text using Tesseract OCR
     images = convert_from_path(pdf_path)
     text = ""
     for img in images:
